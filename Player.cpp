39b60499da5bb00205f0eac65c4e8f6404d0a788
gogen02 <85969838+gogen02@users.noreply.github.com>
2021-06-16 02:04:20 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -14,7 +14,6 @@ Player::Player(QGraphicsItem *parent): QGraphicsPixmapItem(parent){
 }
 
 void Player::keyPressEvent(QKeyEvent *event){
-    // move the player left and right
     if (event->key() == Qt::Key_Left){
         if (pos().x() > 0)
         setPos(x()-10,y());
@@ -23,9 +22,7 @@ void Player::keyPressEvent(QKeyEvent *event){
         if (pos().x() + 100 < 800)
         setPos(x()+10,y());
     }
-    // shoot with the spacebar
     else if (event->key() == Qt::Key_Space){
-        // create a bullet
         Bullet * bullet = new Bullet();
         bullet->setPos(x()+35,y());
         scene()->addItem(bullet);
@@ -52,7 +49,6 @@ void Player::keyPressEvent(QKeyEvent *event){
 }
 
 void Player::spawn(){
-    // create an enemy
     Enemy * enemy = new Enemy();
     scene()->addItem(enemy);
 }
